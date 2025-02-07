be2c5d5f768f291fec1e549dc75b0f262a4bdfa2
Rony-777 <142101857+Rony-777@users.noreply.github.com>
2025-02-07 17:50:32 -0500

    servidor de base cambiada

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 
 const cliente = new Client({
-  connectionString: 'postgresql://rony:t6BmyydCx717mUp4YcB0daZTtGoTkoNA@dpg-ctkqbipopnds7381efq0-a/calidad_aire_wn5n',
+  connectionString: 'postgresql://rony:NK2K20xy3QVUQK0YJ1HS0IcZQbTxraiY@dpg-cuj8oa8gph6c73bd2aog-a/calidad_aire_jep3',
   ssl: {
     rejectUnauthorized: false
   }
