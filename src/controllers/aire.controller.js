e6a9ae4708d3b75bca5c3b284d86c7b8bace51c7
Rony-777 <142101857+Rony-777@users.noreply.github.com>
2025-03-03 21:38:53 -0500

    Endoint de cargar y ver datos agregados, solo funciona mq7, se corrigio el export, se arreglo la fecha de lectura

--- src/controllers/aire.controller.js
+++ src/controllers/aire.controller.js
@@ -2,13 +2,14 @@ const db = require("../db")
 
 const cargarAire = async (req, res) => {
     const {CO_ppm, temp , pm25} = req.body;
-
+    
     try {
         const insertarAire = await db.query(
             "INSERT INTO datos_aire (CO_ppm, temp, pm25) VALUES ($1, $2, $3) RETURNING *",
             [CO_ppm, temp, pm25]
         );
-
+        console.log(  insertarAire.rows[0]);
+        
         res.status(201).json({ message: 'Registro realizado', data: insertarAire.rows[0] });
     } catch (error) {
         res.status(500).json({ message: 'Error en el servidor', error });
